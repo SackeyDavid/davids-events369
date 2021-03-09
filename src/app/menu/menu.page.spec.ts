9758112d05ffae4a788f99c22d87c22614c04562
David <davidkofiahensackey@gmail.com>
2021-03-09 10:18:17 +0000

    ng version updated

--- src/app/menu/menu.page.spec.ts
+++ src/app/menu/menu.page.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MenuPage } from './menu.page';
 
@@ -7,7 +7,7 @@ describe('MenuPage', () => {
   let component: MenuPage;
   let fixture: ComponentFixture<MenuPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MenuPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
